a21520adc9ab429f77e10e80235d89cf00a6fc96
jinbanglin <17golang@gmail.com>
2018-08-23 15:55:59 +0800

    query source

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"github.com/jinbanglin/micro/cmd"
-	// etcdv3 service discover
-	_ "github.com/jinbanglin/go-plugins/registry/etcdv3"
 	// tcp transport
 	_ "github.com/jinbanglin/go-plugins/transport/tcp"
 )
